5c41001bcbcf6c13fc197c8327e291cde3dcaa36
phealy1982 <phealy1982@gmail.com>
2016-10-05 00:01:22 +0100

    first part more

--- src/main/java/net/serenitybdd/dojo/supermarket/model/SupermarketCatalog.java
+++ src/main/java/net/serenitybdd/dojo/supermarket/model/SupermarketCatalog.java
@@ -9,18 +9,19 @@ public class SupermarketCatalog {
     private static final Map<Product, SpecialOffer> OFFERS = new HashMap<>();
 
     public Double priceFor(int quantity, Product product) {
-
-        if (OFFERS.get(product) == SpecialOffer.TWO_FOR_ONE){
+        if (OFFERS.get(product) == SpecialOffer.TWO_FOR_ONE) {
             return applyTwoForOneOffer(quantity, product);
         }
-
         return PRODUCTS.get(product.barcode()) * quantity;
     }
 
     private Double applyTwoForOneOffer(int quantity, Product product) {
-
-        int quantityToPrice = quantity % 2;
-
+        int quantityToPrice = quantity;
+        if (quantity > 1 && quantity % 2 == 0) {
+            quantityToPrice = quantity / 2;
+        } else if (quantity > 1){
+            quantityToPrice = (quantity / 2) + 1;
+        }
         return PRODUCTS.get(product.barcode()) * quantityToPrice;
     }
 
