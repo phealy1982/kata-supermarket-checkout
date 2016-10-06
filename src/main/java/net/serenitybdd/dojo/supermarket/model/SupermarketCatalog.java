fb4457133e81167e7dd5818006b9e75a73614da8
Peter Healy <peter.j.healy@aib.ie>
2016-10-06 17:40:43 +0100

    changes

--- src/main/java/net/serenitybdd/dojo/supermarket/model/SupermarketCatalog.java
+++ src/main/java/net/serenitybdd/dojo/supermarket/model/SupermarketCatalog.java
@@ -5,28 +5,50 @@ import java.util.Map;
 
 public class SupermarketCatalog {
 
-    private static final Map<Barcode, Double> PRODUCTS = new HashMap<>();
+    private static final Map<Barcode, Double> PRICES = new HashMap<>();
     private static final Map<Product, SpecialOffer> OFFERS = new HashMap<>();
 
     public Double priceFor(int quantity, Product product) {
-        if (OFFERS.get(product) == SpecialOffer.TWO_FOR_ONE) {
-            return applyTwoForOneOffer(quantity, product);
-        }
-        return PRODUCTS.get(product.barcode()) * quantity;
+        return priceWithOffersApplied(quantity, product);
     }
 
-    private Double applyTwoForOneOffer(int quantity, Product product) {
+    private Double priceWithOffersApplied(int quantity, Product product) {
+        if (productOnSpecialOffer(product)) {
+            return applySpecialOffer(quantity, product);
+        }
+        return priceFor(product) * quantity;
+    }
+
+    private boolean productOnSpecialOffer(Product product) {
+        return OFFERS.get(product) == SpecialOffer.TWO_FOR_ONE;
+    }
+
+    private Double priceFor(Product product) {
+        return PRICES.get(product.barcode());
+    }
+
+    private Double applySpecialOffer(int quantity, Product product) {
+        SpecialOffer offer = OFFERS.get(product);
         int quantityToPrice = quantity;
+
+        if (offer.equals(SpecialOffer.TWO_FOR_ONE)) {
+            quantityToPrice = applyTwoForOneSpecialOffer(quantity, quantityToPrice);
+        }
+
+        return priceFor(product) * quantityToPrice;
+    }
+
+    private int applyTwoForOneSpecialOffer(int quantity, int quantityToPrice) {
         if (quantity > 1 && quantity % 2 == 0) {
             quantityToPrice = quantity / 2;
-        } else if (quantity > 1){
+        } else if (quantity > 1) {
             quantityToPrice = (quantity / 2) + 1;
         }
-        return PRODUCTS.get(product.barcode()) * quantityToPrice;
+        return quantityToPrice;
     }
 
     public void addWithPrice(Product product, Double price) {
-        PRODUCTS.put(product.barcode(), price);
+        PRICES.put(product.barcode(), price);
     }
 
     public void addSpecialOffer(Product product, SpecialOffer offer) {
